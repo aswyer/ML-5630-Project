b763b8b901b7dd4ff9a4c68e7d5c4dfa09f5810f
Andrew Sawyer <ajsawyer3@gmail.com>
2022-12-01 11:11:59 -0600

    reverted baselines

--- baselines.py
+++ baselines.py
@@ -1,8 +1,13 @@
 ######Imports######
-from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import main
+import random
 import matplotlib.pyplot as plt
+import pandas as pd
 import os
+import cv2
+from pathlib import Path
 from PIL import Image, ImageOps
 from sklearn.utils import Bunch
 from sklearn.model_selection import train_test_split
@@ -19,27 +24,40 @@ emotionFolderNames = [
 		"neutrality", 
 		"sadness",
 		"surprise"
+		# "negative", 
+		# "positive"
 	]
-	
+# 
 fileNames = []
 flatFiles = []
 targets = []
 
-def loadImage(self, emotionFolderName, fileName):
-		path = DATASET_FOLDER_NAME + '/' + emotionFolderName + '/' + fileName
-		
-		image = Image.open(path)
-		image = ImageOps.grayscale(image)
-		array = np.array(image).flatten()
-		array = np.reshape(array, (len(array), 1))
-		normalized = (array / 255) * 2 - 1
-		return normalized
+def load_images():
+	for i, emotion in enumerate(emotionFolderNames):
+		emotionFolderPath = os.getcwd() + '/' + DATASET_FOLDER_NAME + '/' + emotion
+		for name in os.listdir(emotionFolderPath):
+			emotionFolderPath = os.getcwd() + '/' + DATASET_FOLDER_NAME + '/' + emotion
+			if name.endswith('.png') is False: continue
+			path = DATASET_FOLDER_NAME + '/' + emotion + '/' + name
+			image = Image.open(path)
+			image = ImageOps.grayscale(image)
+			array = np.array(image)
+			fileNames.append(array)
+			flatFiles.append(array.flatten())
+			targets.append(i)
+
+	flat_data = np.array(flatFiles)
+	target_array = np.array(targets)
+	image_array = np.array(fileNames)
+
+	return Bunch(data = flat_data, target = target_array, target_names = emotionFolderNames)
+
 	
 image_dataset = load_images()
-X_train, X_test, y_train, y_test = train_test_split(image_dataset.data, image_dataset.target, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(image_dataset.data, image_dataset.target, test_size=0.9, random_state = 109)
 
-nn = MLPClassifier(hidden_layer_sizes=(100,), activation='logistic')
-nn.fit(X_train, y_train)
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
 
 y_pred = rf.predict(X_test)
 mse_score = mean_squared_error(y_test, y_pred)
