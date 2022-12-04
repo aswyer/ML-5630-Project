0bbc0ffdc6a86ff2c82ab630cdd4a3a8963b7cd5
Andrew Sawyer <ajsawyer3@gmail.com>
2022-12-03 23:14:49 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ DATASET_FOLDER_NAME = "dataset_emotion" # dataset_emotion 	| 	dataset_numbers
 INPUT_IMAGE_SIZE = 48					# 48				| 	22
 
 EPOCHS = 1
-MAX_LEARNING_RATE = 1 			# Will be used as constant learning rate if LR_INVERSE_SCALING_ON is false
-LR_INVERSE_SCALING_ON = True 	# Currently doesn't support multiple epochs. will cycle scaling for each epoch.
+MAX_LEARNING_RATE = 0.1 		# Will be used as constant learning rate if LR_INVERSE_SCALING_ON is false
+LR_INVERSE_SCALING_ON = False 	# Currently doesn't support multiple epochs. will cycle scaling for each epoch.
 
 SIZE_HIDDEN_LAYER = 300
 NUM_WEIGHT_SAMPLES = 24 		# Number of weight samples to show in the graph
